ef08bc3cece99cbd41202c0f29846e9b17bdac74
Your Name <thotv.t1.1821@gmail.com>
2022-12-20 04:55:11 +0700

    add router for finding user

--- back-end/src/routes/user.js
+++ back-end/src/routes/user.js
@@ -1,9 +1,17 @@
-const { login, register, getAllContacts } = require("../controllers/user");
+const {
+  login,
+  register,
+  getAllContacts,
+  findByFullname,
+  findByPhone,
+} = require("../controllers/user");
 
 const router = require("express").Router();
 
 router.post("/login", login);
 router.post("/register", register);
 router.get("/get-contacts/:id", getAllContacts);
+router.post("/fullname", findByFullname);
+router.post("/phonhe", findByPhone);
 
 module.exports = router;
