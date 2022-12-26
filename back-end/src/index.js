f69dd8e07beda424c3de2fa7a93346fbc277feb0
H1710 <huyquac@gmail.com>
2022-12-27 01:26:18 +0700

    send-message-socket

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -5,16 +5,40 @@ const DBconnection = require('./DBconnection/db');
 const routes = require('./routes/index');
 const { socketConnect } = require('./models/socket');
 require('dotenv').config();
+const { createServer } = require('http');
+const socket = require('socket.io');
+const { MessageModel } = require('../src/models/mesage');
+const { FriendInvitationModel } = require('../src/models/friendInvitation');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 DBconnection();
-socketConnect();
+// socketConnect();
 
 app.use('/', routes);
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server started on Port ${process.env.PORT}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    credentials: true,
+  },
+});
+
+
+global.onlineUsers = new Map();
+io.on('connection', socket => {
+  console.log('connected');
+  socket.on('join-room', (data) => {
+    socket.join(data);
+  })
+
+  socket.on('send-msg', async data => {
+   socket.to(data.chatRoomId).emit('receive-msg', data);
+  });
 });
